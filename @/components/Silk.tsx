281e4ed17112515e7c3d88b9d33d53142e2e8681
whispersoftheelders <205708280+wote-dev@users.noreply.github.com>
2025-09-09 17:36:43 +1000

    fixed? testing background

--- @/components/Silk.tsx
+++ @/components/Silk.tsx
@@ -1,7 +1,8 @@
 import React, { forwardRef, useMemo, useRef, useLayoutEffect } from 'react';
-import { Canvas, useFrame, useThree, RootState } from '@react-three/fiber';
+import { Canvas, useFrame, useThree } from '@react-three/fiber';
+import type { RootState } from '@react-three/fiber';
 import { Color, Mesh, ShaderMaterial } from 'three';
-import { IUniform } from 'three';
+import type { IUniform } from 'three';
 
 type NormalizedRGB = [number, number, number];
 
